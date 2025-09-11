54206ffebac859d50585b9a65b6b12f355ec73cc
Your Name <ahmedrafifar@gmail.com>
2025-09-11 16:38:10 +0530

    Day 5

---
+++ src/Day5/Pattern2.java
@@ -0,0 +1,17 @@
+package Day5;
+
+import java.util.Scanner;
+
+public class Pattern2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of stars: ");
+        int n=sc.nextInt();
+        for (int i = n; i >=1 ; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*\t");
+            }
+            System.out.println();
+        }
+    }
+}
