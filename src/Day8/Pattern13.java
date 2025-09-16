d67e9f4bb8dcd35f54a00804fc99d09ab19ffdf9
Your Name <ahmedrafifar@gmail.com>
2025-09-16 23:57:52 +0530

    patterns

---
+++ src/Day8/Pattern13.java
@@ -0,0 +1,25 @@
+package Day8;
+
+import java.util.Scanner;
+
+public class Pattern13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int num = 1;
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print(num + " ");
+                num = num * (i - j) / (j + 1);  // Efficient nCr logic
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
